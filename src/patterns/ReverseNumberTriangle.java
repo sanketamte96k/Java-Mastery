918711ac07f89da1136227630865d2d881a9e5f0
Sanket Amte <sanketamte6584@gmail.com>
2026-07-21 11:41:13 +0530

    Complete repeated, reverse and 0-1 triangle patterns

---
+++ src/patterns/ReverseNumberTriangle.java
@@ -0,0 +1,13 @@
+package patterns;
+
+public class ReverseNumberTriangle {
+    public static void main(String[] args){
+
+        for (int i =1; i<=5; i++){
+            for (int j=i; j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
