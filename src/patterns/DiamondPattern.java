16af219985116a5f1636ea8821ae6b545a080842
Sanket Amte <sanketamte6584@gmail.com>
2026-07-23 16:55:27 +0530

    Add Diamond pattern

---
+++ src/patterns/DiamondPattern.java
@@ -0,0 +1,33 @@
+package patterns;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        // Upper Half
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = rows - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
