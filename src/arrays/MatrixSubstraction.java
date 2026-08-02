781f5085a14548cece7651ab56d2dbdc8842b14a
Sanket Amte <sanketamte6584@gmail.com>
2026-08-02 23:11:48 +0530

    Impliment array Subtraction

---
+++ src/arrays/MatrixSubstraction.java
@@ -0,0 +1,33 @@
+package arrays;
+import java.util.Scanner;
+public class MatrixSubstraction {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix1 = new int[3][3];
+        int[][] matrix2 = new int[3][3];
+
+        for (int i=0; i<matrix1.length; i++){
+            for (int j=0; j<matrix1[i].length; j++){
+                System.out.print("Enter Element for Matrix 1 = ");
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        for (int i=0; i<matrix2.length; i++){
+            for (int j=0; j<matrix2[i].length; j++){
+                System.out.print("Enter Element for Matrix 2 =");
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+        int[][] result = new int[3][3];
+
+        for (int i=0; i<result.length; i++){
+            for (int j=0; j< result[i].length; j++){
+
+                result[i][j] = matrix1[i][j] - matrix2[i][j];
+                System.out.print(result[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
