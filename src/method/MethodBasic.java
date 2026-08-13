b09f326292dc34542502bebca6df0eff02882976
Sanket Amte <sanketamte6584@gmail.com>
2026-08-13 21:37:55 +0530

    Start Learning Methods

---
+++ src/method/MethodBasic.java
@@ -0,0 +1,11 @@
+package method;
+
+public class MethodBasic {
+    static void greet(){
+        System.out.println("Welcome Sanket Amte");
+    }
+    public static void main(String[] args){
+        greet();
+    }
+
+}
