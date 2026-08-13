a4580f60188075f7ee4a07cd0fed7bd5e582db01
Sanket Amte <sanketamte6584@gmail.com>
2026-08-13 22:55:11 +0530

    Use Multiple Parameter

--- src/method/twoParameterMethod.java
+++ src/method/twoParameterMethod.java
@@ -2,10 +2,13 @@ package method;
 
 public class twoParameterMethod {
 
-    static void add(int a, int b){
-        System.out.println("Sum = "+(a + b));
+    static void studentmark(int math, int java, int DBMS){
+        System.out.println("Math ="+math);
+        System.out.println("Java ="+java);
+        System.out.println("DBMS ="+DBMS);
+        System.out.println("Total ="+(math+java+DBMS));
     }
     public static void main(String[] args){
-        add(10,20);
+        studentmark(95,90,93);
     }
 }
