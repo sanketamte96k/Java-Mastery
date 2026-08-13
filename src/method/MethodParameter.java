07bbde933ade8e68cf983ac7eeb043fc2149a71d
Sanket Amte <sanketamte6584@gmail.com>
2026-08-13 22:37:45 +0530

    Print Table Using Method

--- src/method/MethodParameter.java
+++ src/method/MethodParameter.java
@@ -2,11 +2,11 @@ package method;
 
 public class MethodParameter {
     static void PrintNumber(int n){
-        for (int i=1; i<=n; i++){
-            System.out.println(i);
+        for (int i=1; i<=10; i++){
+            System.out.println(n+" X "+i+" = "+(n*i));
         }
     }
     public static void main(String[] args){
-        PrintNumber(10);
+        PrintNumber(2);
     }
 }
