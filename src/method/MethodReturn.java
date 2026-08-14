02a3bb3e5b5382c61892ba9774e73a26a9452212
Sanket Amte <sanketamte6584@gmail.com>
2026-08-14 14:14:28 +0530

    Impliment Return in Methods

---
+++ src/method/MethodReturn.java
@@ -0,0 +1,13 @@
+package method;
+
+public class MethodReturn {
+
+    static int getNumber(){
+        return 10;
+    }
+    public static void main(String[] args){
+        int result = getNumber();
+
+        System.out.println(result);
+    }
+}
