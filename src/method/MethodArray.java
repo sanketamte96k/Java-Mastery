afc4540e6055042a527a7d2f8c1bdcd52fba975b
Sanket Amte <sanketamte6584@gmail.com>
2026-08-15 11:18:57 +0530

    Impliment Array in Method

---
+++ src/method/MethodArray.java
@@ -0,0 +1,13 @@
+package method;
+
+public class MethodArray {
+    static void printArray(int[] arr ) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args){
+        int[] result = {10, 20, 30, 40};
+        printArray(result);
+    }
+}
