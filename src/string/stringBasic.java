ab51f941fea12a1ddd8ed0a19ace649e63a6bb7f
Sanket Amte <sanketamte6584@gmail.com>
2026-08-26 10:24:21 +0530

    Find String Value at specific index

--- src/string/stringBasic.java
+++ src/string/stringBasic.java
@@ -3,6 +3,6 @@ package string;
 public class stringBasic {
     public static void main(String[] args){
         String Name = "Sanket";
-        System.out.println(Name.length());
+        System.out.println(Name.charAt(1));
     }
 }
