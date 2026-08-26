ca354f9f623316f98e0beaca17044a11aadb76e9
Sanket Amte <sanketamte6584@gmail.com>
2026-08-26 19:30:29 +0530

    Impliment StringBuilder

---
+++ src/string/StringBuilderBasic.java
@@ -0,0 +1,11 @@
+package string;
+
+public class StringBuilderBasic {
+    public static void main(String[] args){
+        java.lang.StringBuilder sb = new java.lang.StringBuilder();
+
+        sb.append("Java");
+
+        System.out.println(sb.reverse());
+    }
+}
