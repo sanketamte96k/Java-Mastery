bb8cb3f368585ec75ac36a417dd83948915171b6
Sanket Amte <sanketamte6584@gmail.com>
2026-08-26 18:40:40 +0530

    Impliment for loop in String

---
+++ src/string/StringLoop.java
@@ -0,0 +1,11 @@
+package string;
+
+public class StringLoop {
+    public static void main(String[] args){
+        String Name = "Sanket";
+
+        for (int i=0; i< Name.length(); i++){
+            System.out.println(Name.charAt(i));
+        }
+    }
+}
--- src/string/stringBasic.java
+++ src/string/stringBasic.java
@@ -4,5 +4,8 @@ public class stringBasic {
     public static void main(String[] args){
         String Name = "Sanket";
         System.out.println(Name.charAt(1));
+        System.out.println(Name.charAt(3));
+        System.out.println(Name.charAt(5));
+        System.out.println(Name.charAt(2));
     }
 }
