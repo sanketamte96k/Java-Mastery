de91afb5c94c629086e10c690f8ed2fe4364235c
Sanket Amte <sanketamte6584@gmail.com>
2026-08-26 10:13:46 +0530

    Print Name

--- src/string/stringBasic.java
+++ src/string/stringBasic.java
@@ -1,4 +1,12 @@
 package string;
 
 public class stringBasic {
+    static void fullName(){
+        String Name = "Sanket";
+        String LastName = "Amte";
+        System.out.println(Name+" "+LastName);
+    }
+    public static void main(String[] args){
+        fullName();
+    }
 }
