719a37b31a38c22f2c6754d6554a15a3cfe92251
Sanket Amte <sanketamte6584@gmail.com>
2026-09-22 18:46:29 +0530

    Add info in class

--- src/oops/studentinfo.java
+++ src/oops/studentinfo.java
@@ -2,6 +2,12 @@ package oops;
 
 public class studentinfo {
     public static void main(String[] args){
+        String name = "Sanket";
+        int age = 21;
+        int rollNo = 2104;
+        String course = "B.tech";
+
+        System.out.println(age);
 
     }
 }
