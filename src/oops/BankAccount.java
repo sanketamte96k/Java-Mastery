7c69bed5236c0135c770cd6914a0b48138d36f1b
Sanket Amte <sanketamte6584@gmail.com>
2026-09-22 23:54:15 +0530

    Add constructor in classes

--- src/oops/BankAccount.java
+++ src/oops/BankAccount.java
@@ -2,32 +2,34 @@ package oops;
 
 public class BankAccount {
     public static void main(String[] args){
-        Account b1 = new Account();
-        Account b2 = new Account();
-        b1.accountHolder = "Sanket";
-        b2.accountHolder = "Sachin";
-        b1.accountNumber = 746;
-        b2.accountNumber = 846;
-        b1.balance = 400;
-        b2.balance = 800;
+        Account b1 = new Account("Sanket",768,500);
+        Account b2 = new Account("Rohan",657,600);
 
         b1.displayBalance();
         b2.displayBalance();
-
+        System.out.println(Account.bankName);
     }
 }
 class Account{
+    static String bankName = "HDFC Bank";
     String accountHolder;
     int accountNumber;
     double balance;
 
-    void deposit(){
-        System.out.println("Money Added");
+    // use constructor
+    Account(String accountHolder, int accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
     }
-    void withdraw(){
-        System.out.println("Money Withdraw done");
+
+        void deposit () {
+            System.out.println("Money Added");
+        }
+        void withdraw () {
+            System.out.println("Money Withdraw done");
+        }
+        void displayBalance () {
+            System.out.println("Balance = " + balance);
+        }
     }
-    void displayBalance(){
-        System.out.println("Balance = "+balance);
-    }
-}
