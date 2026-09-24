a27f37b1bfcd680879191cd5290d75490458f64b
Sanket Amte <sanketamte6584@gmail.com>
2026-09-24 19:08:32 +0530

    Complete MethodOverloading

--- src/oops/MethodOverloading.java
+++ src/oops/MethodOverloading.java
@@ -3,15 +3,23 @@ package oops;
 public class MethodOverloading {
     public static void main(String[] args){
         Add a1 = new Add();
-        a1.add(3,2);
-        a1.add(2,4,6);
+        a1.add(12,24);
+        a1.add("Sanket","Amte");
+        a1.displauInfo(21,"Sanket");
+        a1.displayInfo("Sanket",21);
     }
 }
 class Add{
-    void add(int a, int b){
+    void add(double a, double b){
         System.out.println("Sum = "+(a + b));
     }
-    void add(int a, int b, int c){
-        System.out.println("Sum = "+(a + b + c));
+    void add(String a , String b){
+        System.out.println(a +" "+b);
+    }
+    void displayInfo(String a, int b){
+        System.out.println("Name = "+ a +" Age = "+b);
+    }
+    void displauInfo(int a, String b){
+        System.out.println("Age = "+a+" Name = "+b);
     }
 }
